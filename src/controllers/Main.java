d6515cb5367681fdac4b5f0c786eae39bd61f4e6
ZohaibJabbar <ZohaibJabbar@ZohaibJabbar-PC>
2015-09-21 17:57:16 +0100

    3rd commit

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,14 +1,20 @@
 package controllers;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import models.User;
 import utils.FileLogger;
 
 public class Main
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws IOException
   {
     FileLogger logger = FileLogger.getLogger();
     logger.log("Creating user list");
@@ -18,6 +24,12 @@ public class Main
     users.add(new User("Homer", "Simpson", "bart@simpson.com", "secret"));
     users.add(new User("Lisa", "Simpson", "bart@simpson.com", "secret"));
     System.out.println(users);
+    
+    logger.log("Serializing contacts to XML");
+    XStream xstream = new XStream(new DomDriver());
+    ObjectOutputStream out = xstream.createObjectOutputStream(new FileWriter("users.xml"));
+    out.writeObject(users);
+    out.close();    
     
     logger.log("Finished - shutting down");
     
