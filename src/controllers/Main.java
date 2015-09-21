b08adc7d7d68441820fc13263a31c03435893368
ZohaibJabbar <ZohaibJabbar@ZohaibJabbar-PC>
2015-09-21 17:53:31 +0100

    2nd commit

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -4,15 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import models.User;
+import utils.FileLogger;
 
 public class Main
 {
   public static void main(String[] args)
   {
+    FileLogger logger = FileLogger.getLogger();
+    logger.log("Creating user list");
+
     List<User> users = new ArrayList<User>();
     users.add(new User("Bart", "Simpson", "bart@simpson.com", "secret"));
     users.add(new User("Homer", "Simpson", "bart@simpson.com", "secret"));
     users.add(new User("Lisa", "Simpson", "bart@simpson.com", "secret"));
     System.out.println(users);
+    
+    logger.log("Finished - shutting down");
+    
   }
 }
