b88d5d14fee6b20b27cfac4e3e54ddae58e7414a
ZohaibJabbar <zohaibjabbar@gmail.com>
2015-10-04 09:54:35 +0100

    lab2 -05

---
+++ src/controllers/pacemakerAPI.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import models.User;
+
+public class pacemakerAPI
+{
+  private Map<String, User> users = new HashMap<String, User>();
+
+  public Collection<User> getUsers ()
+  {
+    return users.values();
+  }
+
+  public  void deleteUsers() 
+  {
+    users.clear();
+  }
+
+  public User createUser(String firstName, String lastName, String email, String password) 
+  {
+    User user = new User (firstName, lastName, email, password);
+    users.put(email, user);
+    return user;
+  }
+
+  public User getUser(String email) 
+  {
+    return users.get(email);
+  }
+
+  public void deleteUser(String email) 
+  {
+    users.remove(email);
+  }
+}
